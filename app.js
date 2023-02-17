97e65644963d1aad047b94edce4ee2b55f2ed0ad
haifeng <haifeng.wang@se.com>
2023-02-17 13:36:10 +0800

    test render runtime env

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
+const env = process.env;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`app env:\n ${env}\n`));
 
 
 const html = `
