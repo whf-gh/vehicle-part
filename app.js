04091871eb585f8ce5d61ff7c8a6100d82195b69
haifeng <haifeng.wang@se.com>
2023-02-17 13:42:53 +0800

    env test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
-const env = process.env;
+const env = JSON.stringify(process.env);
 
 app.get("/", (req, res) => res.type('html').send(html));
 
