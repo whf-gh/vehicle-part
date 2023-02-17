9927ec7bcc79c46286bdef409f249a9e3c4bd342
haifeng <haifeng.wang@se.com>
2023-02-17 14:31:41 +0800

    switch to vehicle-part app

--- app.js
+++ app.js
@@ -1,61 +1,94 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-const env = JSON.stringify(process.env);
+var express = require('express');
+var app = express();
+const port = process.env.PORT || 8081;
+const extern_url = process.env.RENDER_EXTERN_URL || `http://127.0.0.1:${port}`;
 
-app.get("/", (req, res) => res.type('html').send(html));
+var htxt;
+var txtf="style='font-size:50px'";
+var txt0=`<html><head><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'></head><body><form action='${extern_url}/search' style='font-size:50px' method='GET'>`;
+var txt1=`<html><body><meta http-equiv='Content-Type' content='text/html; charset=UTF-8'><form action='${extern_url}/pay' style='font-size:50px' method='GET'>`;
+var txt2='<br><br>Pleaes input the last several signs of your vehicle license number.';
+var txt3="<br><input type='text' style='font-size:50px' name='VL ";
+var txt4="' value='";
+var txt5;
+var txt6="'>"
+var txt7="<input type='submit' style='font-size:50px' value='Search'></form></body></html>";
+var txt8="<input type='submit' style='font-size:50px' value='Pay'></form></body></html>";
 
-//app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.listen(port, () => console.log(`app env:\n ${env}\n`));
+//app.use(express.static('public'));
+ 
+var server = app.listen(port, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log(`listen at ${extern_url}:${port}`);
+})
 
+var io = require('socket.io')(server);
+var socket_obj;
+var socket_connected=false;
+var res_obj;
+var cnt=0;
 
-const html = `
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Hello from Render!</title>
-    <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
-    <script>
-      setTimeout(() => {
-        confetti({
-          particleCount: 100,
-          spread: 70,
-          origin: { y: 0.6 },
-          disableForReducedMotion: true
-        });
-      }, 500);
-    </script>
-    <style>
-      @import url("https://p.typekit.net/p.css?s=1&k=vnd5zic&ht=tk&f=39475.39476.39477.39478.39479.39480.39481.39482&a=18673890&app=typekit&e=css");
-      @font-face {
-        font-family: "neo-sans";
-        src: url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"), url("https://use.typekit.net/af/00ac0a/00000000000000003b9b2033/27/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
-        font-style: normal;
-        font-weight: 700;
-      }
-      html {
-        font-family: neo-sans;
-        font-weight: 700;
-        font-size: calc(62rem / 16);
-      }
-      body {
-        background: white;
-      }
-      section {
-        border-radius: 1em;
-        padding: 1em;
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        margin-right: -50%;
-        transform: translate(-50%, -50%);
-      }
-    </style>
-  </head>
-  <body>
-    <section>
-      Hello from Render!
-    </section>
-  </body>
-</html>
-`
+io.on('connection', function(socket){
+	console.log("io connected" + cnt++);
+	socket_obj=socket;
+  	socket_connected=true;
+  	socket.on('found', function(message){
+    	console.log('found'+message);
+    	var object = JSON.parse(message.toString());
+    	var inf=object.inf;
+    	var num=object.num;
+    	var txt=txt1+inf+txt3+txt4+num+txt6+txt8;
+    	res_obj.end(txt);
+    	console.log('found'+num);
+  	});
+  	socket.on('unfound', function(message){
+    	console.log('unfound'+message);
+    	var object = JSON.parse(message.toString());
+    	var inf=object.inf;
+    	var num=object.num;
+    	var txt=txt0+num+' is not found'+txt2+txt3+txt6+txt7;
+    	res_obj.end(txt);  		
+    	console.log('unfound'+num);
+  	});
+  	socket.on('payed', function(message){
+    	console.log('payed'+message);
+  		var object = JSON.parse(message.toString());
+    	var inf=object.inf;
+    	var num=object.num;
+    	var txt=txt0+'Thank you for your payment.'+txt2+txt3+txt6+txt7;
+    	res_obj.end(txt);  	
+
+    	console.log('payed'+num);
+  	})
+  	socket.on('unpayed', function(message){
+    	console.log('unpayed'+message);
+    	var object = JSON.parse(message.toString());
+    	var inf=object.inf;
+    	var num=object.num;
+    	var txt=txt0+num+' is not found. Payment fails.'+txt2+txt3+txt6+txt7;
+    	res_obj.end(txt);  
+
+    	console.log('unpayed'+num);
+  	})
+});
+
+app.get('/search', function (req, res) {
+   	if (socket_connected){
+      	socket_obj.emit('VL',req.query.VL);
+      	res_obj=res;
+   	}
+})
+
+app.get('/pay', function (req, res) {
+   	if (socket_connected){
+      	socket_obj.emit('Pay',req.query.VL);
+      	res_obj=res;
+   	}  
+})
+
+app.get('/', function (req, res) {
+   	var txt=txt0+txt2+txt3+txt6+txt7;
+   	res.end(txt);  
+   	console.log('home');
+})
